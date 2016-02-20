d0d2645a124cad610fc0bb51f1b5ba08ff5b65d0
Miles Florence <milesflo15@gmail.com>
2016-02-19 16:42:36 -0800

    app.module

--- js/app.js
+++ js/app.js
@@ -1,25 +1,16 @@
-$(document).ready(function() {
-    $('select').material_select();
-});
+var app = angular.module("eventTracker", ["ngRoute", "ngAnimate"]);
 
-
-var app = angular.module("meanTea", ["ngRoute"]);
-
-app.filter('cents', function($filter) {
-	return function (input) {
-		return (input/100);
-	}
-})
-
-app.config(function($routeProvider, $locationProvider) {
+app.config(function($routeProvider, $http) {
 	$routeProvider
 	.when('/', {
-		templateUrl: "partials/catalogue.html",
-		controller: "Catalogue"
-	}).when('/cart', {
-		templateUrl: "partials/cart.html",
-		controller: "Cart"
-	}).otherwise({
+		templateUrl: "partials/landing.html",
+		controller: "Landing"
+	}).when('/event', {
+		templateUrl: "partials/event.html"
+		controller: "Landing"
+	})
+	.otherwise({
 		redirectTo: "/"
 	})
 })
+
