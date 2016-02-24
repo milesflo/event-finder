b8499e1497c0f37b4360e608782493acaf2cd5bf
Miles Florence <milesflo15@gmail.com>
2016-02-24 14:36:08 -0800

    fixed accidental delete

---
+++ server/server.js
@@ -0,0 +1,80 @@
+var express 			= require("express"),
+	app 				= express(),
+	bodyParser 			= require("body-parser"),
+	morgan 				= require("morgan"),
+	routes 				= require('./routes'),
+	path 				= require('path'),
+	knex 				= require('../db/knex'),
+	passport 			= require('passport'),
+	FacebookStrategy	= require('passport-facebook').Strategy,
+    dotenv              = require('dotenv').load();
+var worker = require('./worker.js');
+app.use(passport.initialize());
+
+passport.serializeUser(function(user, done) {
+	if(user[0] === undefined){
+		done(null, user);
+	} else {
+		done(null, user[0]);
+	}
+});
+
+passport.deserializeUser(function(user, done) {
+	Knex('users').where({ id: user.id }).then(function(user, err) {
+		done(err, user);
+	});
+});
+
+
+passport.use(new FacebookStrategy({
+	clientID: process.env.FBCLIENTID,
+	clientSecret: process.env.FBCLIENTSECRET,
+	callbackURL: 'http://localhost:3000/auth/facebook/callback'
+	},
+	function(token, refreshToken, profile, done) {
+		console.log(profile + "HERE!!!!");
+		process.nextTick(function() {
+			Knex('users').where({facebook_id: profile.id}).then(function(user, err) {
+				if(err)
+					done(err);
+				if(user[0]) {
+					return done(null, user[0]);
+				} else {
+					knex('users').insert({facebook_id: profile.id, username: profile.displayName}).then(function() {
+						knex('users').where({facebook_id: profile.id}).then(function(data) {
+							return done(null, data[0]);
+						});
+					});
+				}
+			});
+		});
+	}
+));
+
+
+
+app.use('/client', express.static(path.join(__dirname, '../client')));
+app.use('/js',express.static(path.join(__dirname, '../client/js')));
+app.use('/templates',express.static(path.join(__dirname, '../client/js/templates')));
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use('/api/users', routes.users);
+
+app.get('/', function(req,res){
+	res.sendFile(path.join(__dirname,'../client/views', 'index.html'));
+});
+
+app.get('/apiGet', function(req,res) {
+	worker.eventFulSearch(req.query);
+})
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/users.js')(app,passport);
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {console.log("Listening on localhost:", PORT)});
