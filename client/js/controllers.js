c06d0d8e9041823e7d214f10a7e06a76aa11e0fc
sirinapha88 <sirinapha888@gmail.com>
2016-02-23 16:34:05 -0800

    modified controllers.js

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -47,14 +47,9 @@ app.controller('LoginController', function($scope, $http) {
 });
 
 app.controller('ApiCtrl', function($scope, $http, $timeout){
-	$timeout(function() { FB.api('/search', 'GET', {q: 'fun', type: 'event'}, function(data) { console.log(data)});
-    $http.get('https://www.eventbriteapi.com/v3/events/search/?q=fun&token=JIOZSXTUJLB6JV62IK5U').success(function(data){
-        console.log(data);
-    });
-    FB.api('/me',{
-        fields: 'first_name'
-    }, function(data) {
-        console.log(data)
-	})
-	}, 2000)
+	
+    // $http.get('https://www.eventbriteapi.com/v3/events/search/?q=fun&token=JIOZSXTUJLB6JV62IK5U').success(function(data){
+    //     console.log(data);
+    // });
+    
 });
