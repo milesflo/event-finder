e1ea95795db724b30475966a0e4fd2bee8d4332f
sirinapha88 <sirinapha888@gmail.com>
2016-02-24 16:43:42 -0800

    made change migration file

--- migrations/20160224113224_user_events.js
+++ migrations/20160224113224_user_events.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('user_events', function(table){
     table.increments();
     table.integer('user_id').references('id').inTable('users').onDelete('cascade');
-    table.integer('event_id').references('id').inTable('events').onDelete('cascade');
+    table.json('eventJson');
     table.boolean('hasDeleted');
   });
 };
--- server/routes/eventBrite.js
+++ server/routes/eventBrite.js
@@ -5,7 +5,7 @@ var express = require('express'),
 
 
 app.get('/', function(req, res) {
-	request('https://www.eventbriteapi.com/v3/events/17920884849/?token=J7WH6466CFBTZQZ43RJP' , function(err, response, body){
+	request.get('https://www.eventbriteapi.com/v3/events/17920884849/?token=J7WH6466CFBTZQZ43RJP' , function(err, response, body){
 		console.log(JSON.parse(body));
 		var myData = JSON.parse(body);
 		
