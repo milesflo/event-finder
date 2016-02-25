367611b873677296c968c9cab7d9a41ee0de8fb4
Miles Florence <milesflo15@gmail.com>
2016-02-25 15:22:26 -0800

    Added Server->Database->API functionality

--- server/worker.js
+++ server/worker.js
@@ -1,41 +1,63 @@
-eventfulSearch = function(query) {
+var knex = require("./../db/knex.js");
+var promise = require("bluebird");
+var dateFormat = require('dateformat');
+
+eventfulSearch = function(query, done) {
     var eventful = require('eventful-node')
     var client = new eventful.Client("QfMJMhDnc8BXXWC6");
 
-	client.searchEvents({   keywords: query.q,
+	client.searchEvents({   keywords: query.query,
                             location: "San Francisco"
-                        }, function(err, data){
-        if(err){
+                        }, 
+        function(err, data){
+            if(err){
 
-            done(err, null);
+                done(err, null);
 
-        }
-        done(null, data);
+            }
+            done(null, data);
 
-        var insertPromises = [];
+            var insertPromises = [];
 
-        for (var i = 0; i < data.search.events.event.length; i++) {
-            var event = {
-                "event_name" : data.search.events.event[i].title,
-                "event_desc" : data.search.events.event[i].description,
-                "latitude"   : data.search.events.event[i].latitude,
-                "longitude"  : data.search.events.event[i].longitude,
-                "start_time" : data.search.events.event[i].start_time,
-                "end_time"   : data.search.events.event[i].stop_time,
-                "eventJson"  : data.search.events.event[i]
+            if (data && data.search && data.search.total_items >0) {
 
+               for (var i = 0; i < data.search.events.event.length; i++) {
+                    var event = {
+                        "event_name" : data.search.events.event[i].title,
+                        "event_desc" : data.search.events.event[i].description,
+                        "latitude"   : data.search.events.event[i].latitude,
+                        "longitude"  : data.search.events.event[i].longitude,
+                        "start_time" : dateFormat(data.search.events.event[i].start_time,"isoDateTime"),
+                        "end_time"   : dateFormat(data.search.events.event[i].stop_time,"isoDateTime"),
+                        "eventJson"  : data.search.events.event[i]
+
+                    };
+                    knex("user_events").insert(event).then(function(response) {
+                        //console.log("hello");
+                        //console.log(response);
+                    }).catch(function(response) {
+                        //console.log("goodbye");
+                        //console.log(response);
+                    });
+                } 
             };
-            insertPromises.push(knex("user_events").insert(event));
-        }
-        console.log(insertPromises);
-        insertPromises.every(function (result) {
-            console.log(result);
-            //console.log("output\n", result);
-        })
+            knex('queue').update({done: true}).where({id: query.id}).then(function(){});
     });
 };
 
+function processQueue() {
+    knex('queue').whereNot({done: true}).then(function(queries) {
+        for (var i = 0; i < queries.length; i++) {
+                console.log("hello", queries[i]);
+            eventfulSearch(queries[i], function(result) {
+            });
+        }
+    }).catch(function(err){console.log(err)});
+}
+
+setInterval(processQueue,1000);
 
 module.exports = {
-    eventFulSearch: eventfulSearch,
+    eventFulSearch : eventfulSearch,
+    processQueue   : processQueue
 }
